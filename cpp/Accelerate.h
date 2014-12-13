3be8cbd0647773293266e49cbfd1abc6ec019c93
Arthur Carabott <arthurc@gmail.com>
2014-12-13 13:30:11 +0000

    remove accel func extra 1

--- cpp/Accelerate.h
+++ cpp/Accelerate.h
@@ -15,12 +15,12 @@ typedef	unsigned long Length;
 typedef long Stride;
 
 
-extern	void vDSP_vclr1(
+extern	void vDSP_vclr(
 			  float *       __vDSP_C,
 			  Stride   __vDSP_K,
 			  Length   __vDSP_N);
 
-extern void vDSP_vsdiv1 (
+extern void vDSP_vsdiv(
    float *__vDSP_A,
    Stride __vDSP_I,
    float *__vDSP_B,
@@ -29,21 +29,21 @@ extern void vDSP_vsdiv1 (
    Length __vDSP_N
 );
 
-extern	void vDSP_vflt161(
+extern	void vDSP_vflt16(
 				short *       A,
 				Stride   __vDSP_I,
 				float *       __vDSP_C,
 				Stride   __vDSP_K,
 				Length   __vDSP_N);
 
-extern	void vDSP_vramp1(
+extern	void vDSP_vramp(
 			   float *       __vDSP_A,
 			   float *       __vDSP_B,
 			   float *       __vDSP_C,
 			   Stride   __vDSP_K,
 						Length   __vDSP_N);
 
-extern	void vDSP_vmul1(
+extern	void vDSP_vmul(
 				   const float   __vDSP_input1[],
 				   Stride   __vDSP_stride1,
 				   const float   __vDSP_input2[],
@@ -52,7 +52,7 @@ extern	void vDSP_vmul1(
 				   Stride   __vDSP_strideResult,
 				   Length   __vDSP_size);
 
-extern	void vDSP_vadd1(
+extern	void vDSP_vadd(
 			  const float   __vDSP_input1[],
 			  Stride   __vDSP_stride1,
 			  const float   __vDSP_input2[],
@@ -61,7 +61,7 @@ extern	void vDSP_vadd1(
 			  Stride   __vDSP_strideResult,
 				   Length   __vDSP_size);
 
-extern	void vDSP_vsma1(
+extern	void vDSP_vsma(
 			  const float *  __vDSP_A,
 			  Stride    __vDSP_I,
 			  const float *  __vDSP_B,
@@ -71,19 +71,19 @@ extern	void vDSP_vsma1(
 			  Stride    __vDSP_L,
 			Length    __vDSP_N);
 
-extern	void vDSP_meanv1(
+extern	void vDSP_meanv(
 			   float *       __vDSP_A,
 			   Stride   __vDSP_I,
 			   float *       __vDSP_C,
 					Length   __vDSP_N);
 
-extern void vDSP_vfill1(
+extern void vDSP_vfill(
 		   float *       __vDSP_A,
 		   float *       __vDSP_C,
 		   Stride   __vDSP_K,
 					   Length   __vDSP_N);
 
-extern void vDSP_vsmul1(
+extern void vDSP_vsmul(
 		   const float    __vDSP_input1[],
 		   Stride    __vDSP_stride1,
 		   const float *  __vDSP_input2,
@@ -91,7 +91,7 @@ extern void vDSP_vsmul1(
 		   Stride    __vDSP_strideResult,
 		   Length    __vDSP_size);
 
-extern void vDSP_vclip1(
+extern void vDSP_vclip(
 		   float *       __vDSP_A,
 		   Stride   __vDSP_I,
 		   float *       __vDSP_B,
